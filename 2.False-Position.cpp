7a4045089ab7381fbbb5b1018d2937619c2ada90
RAI <immaturedeath123@gmail.com>
2025-08-19 22:10:07 +0545

    RAI

---
+++ 2.False-Position.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define f(x) (x*x-x-2)
+#define E 0.001
+int main()
+{
+	float x1,x2,f1,f2,x0,f0;
+	lbl:
+		printf("\nEnter the two initial guess values: ");
+		scanf("%f%f",&x1,&x2);
+		f1 = f(x1);
+		f2 = f(x2);
+		if(f1*f2>=0)
+		{
+			printf("Incorrect guess values... choose next guess value");
+			goto lbl;
+		} 
+		else
+		{
+			lbl2:
+				x0 = x1-(f1*(x2-x1))/(f2-f1);
+				f0 = f(x0);
+				if(f0==0)
+				{
+					printf("The root is: %f", x0);
+				//	goto lbl1;
+				}
+				else if(f0*f1<0)
+				{
+					x2 = x0;
+				}
+				else
+				{
+					x1 = x0;
+				}
+				if(fabs((x2-x1)/x2)<=E)
+				{
+					printf("The root is: %f",x0);
+				}
+				else
+				{
+					goto lbl2;
+				}
+		}
+	return 0;
+}
